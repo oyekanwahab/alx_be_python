62dc1948c28c2e9def88a9b396c439a646d6e8a8
abdul-wahab oyekan <oyekanwahab@gmail.com>
2025-07-13 22:11:05 +0000

    commit for saving

---
+++ finance_calculator.py
@@ -0,0 +1,8 @@
+income = int(input(Enter your monthly income: ))
+expenses = int(input(Enter your total monthly expenses: ))
+
+monthly_saving = income - expenses
+Projected_Savings = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
+
+print(f"Your monthly savings are ${monthly_saving}")
+print(f"Projected savings after one year, with interest, is: ${Projected_Savings}")
