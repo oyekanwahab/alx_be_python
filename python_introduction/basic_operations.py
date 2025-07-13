a7705f16440908c5a4c3711f8312f5a4539cd312
abdul-wahab oyekan <oyekanwahab@gmail.com>
2025-07-13 21:37:26 +0000

    push for basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+sub = number1 - number2
+Mult = number1 * number2
+print(f"Additon of {number1} and {number2} is {add}")
+print(f"Subraction of {number1} and {number2} is {sub}")
+print(f"Multiplication of {number1} and {number2} is {Mult}")
