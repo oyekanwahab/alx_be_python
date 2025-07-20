ba0e7289276fd42046d9cc765bfef0579c3aa422
abdul-wahab oyekan <oyekanwahab@gmail.com>
2025-07-20 22:26:08 +0000

    commit for task 2

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,20 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the decond number: "))
+opt = input("Choose the operation (+, -, *, /): ").lower()
+resul = 0 
+match opt:
+    case "+":
+        result = num1 + num2
+        print(f"The result is {result}")
+    case "-":
+        result = num1 - num2
+        print(f"The result is {result}")
+    case "*":
+        result = num1 * num2
+        print(f"The result is {result}")
+    case "/":
+        if num1 == 0 or num2 == 0:
+            print("Cannot divide by zero.")
+        else :
+            result = num1 / num2
+            print(f"The result is {result}")
