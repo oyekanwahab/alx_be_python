c3c339a1b81e21064dbb54d336fcc03e9ca0de5f
abdul-wahab oyekan <oyekanwahab@gmail.com>
2025-07-20 22:05:09 +0000

    commmit for task 1

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+question = input("What's the weather like today? (sunny/rainy/cold):").lower()
+if question == "sunny":
+    print("Wear a t-shirt and sunglasses")
+elif question == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif question == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
+else :
+    print("Sorry, I don't have recommendations for this weather. ")
