47ed6687efe6b79fb1efe726cd2e8f5687a8ccaa
abdul-wahab oyekan <oyekanwahab@gmail.com>
2025-07-27 19:01:47 +0000

    commit 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,6 +9,6 @@ def perform_operation(num1, num2, operation):
         case 'divide':
             if num2 == 0:
                 quit
-            else:
+            elif:
                  return num1 / num2
         
